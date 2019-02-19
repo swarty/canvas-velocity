07325f20604ff6761349dfbf7ae25a3f884b47f4
d.ogorodnykov <d.ogorodnikov@lestro.com>
2019-02-19 17:04:16 +0200

    update canvas practise

---
+++ main-dist.js
@@ -0,0 +1,50 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	var canvasWr = document.querySelector('.canvas__wr');
+	var canvas = document.createElement('canvas');
+
+	// canvas style settings
+	canvas.style.position = 'absolute';
+	canvas.style.top = '0';
+	canvas.style.left = '0';
+	canvas.style.backgroundImage = 'url("./net.png")';
+	canvas.style.width = '100%';
+	canvas.style.height = '100%';
+
+	// canvas append element to document
+	canvas.width = canvasWr.clientWidth;
+	canvas.height = canvasWr.clientHeight;
+	canvasWr.appendChild(canvas);
+
+	var ctx = canvas.getContext('2d');
+	ctx.lineWidth = 10;
+
+	function drawFunction(e) {
+		var x = e.offsetX,
+		    y = e.offsetY;
+		ctx.beginPath();
+		ctx.fillRect(x - 5, y - 5, 10, 10);
+	}
+
+	function removeDrawOnMouse() {
+		canvas.removeEventListener('mousemove', drawFunction);
+	}
+
+	// event on draw
+	canvas.addEventListener('click', drawFunction);
+	canvas.addEventListener('mousedown', function () {
+		ctx.fillStyle = 'rgb(' + getRandomInt(0, 256) + ',' + getRandomInt(0, 256) + ',' + getRandomInt(0, 256) + ')';
+		canvas.addEventListener('mousemove', drawFunction);
+	});
+
+	// event on stop draw
+	canvas.addEventListener('mouseup', removeDrawOnMouse);
+	canvas.addEventListener('mouseleave', removeDrawOnMouse);
+
+	// random function
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
+});
